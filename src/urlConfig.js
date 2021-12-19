6e1f6da1e6085d5e9415a4c7d7387d9261bb2964
priya6100 <priyaranajan@grapsnextsocile.com>
2021-12-19 13:04:10 +0530

    base url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,12 @@
-export const api = 'http://localhost:7000/api';
+const baseUrl =
+location.hostname === "localhost"
+  ? "http://localhost:2000"
+  : "https://soulbackend.herokuapp.com";
+
+
+
+export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) =>{
 
-    return `http://localhost:7000/public/${fileName}`;
-}
+    return `${baseUrl}/public/${fileName}`;
+};
